ec92c929a3622df61185a048787a2297bfd6e849
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-01-02 04:06:37 +0000

    Add function to call correct calculation functions.
    The operate funciton accepts two numbers and an operator to determine which calculation to run.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let total;
+
 function add(a, b) {
     total = a + b
     return total
@@ -15,5 +17,23 @@ function multiply(a, b){
 
 function divide(a, b){
     total = a / b
+    return total
+}
+
+function operate(operator, a, b){
+    switch (operator){
+        case "+":
+            add(a, b)
+            break;
+        case "-":
+            subtract(a, b)
+            break;
+        case "x":
+            multiply(a, b)
+            break;
+        case "&#247":
+            divide(a, b)
+            break;
+    }
     return total
 }
