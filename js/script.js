8abd4af449bf705bbcaa57548d68b62da5e8b615
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-01-02 06:19:51 +0000

    Add a function to check for an ongoing caluclation
    calcCheck will check whether there is an ongoing calculation and save the total if true.

--- js/script.js
+++ js/script.js
@@ -2,80 +2,136 @@ const calcNumbers = document.querySelectorAll(".button-num")
 const operators = document.querySelectorAll(".button-operator")
 const equalsButton = document.querySelector("#equals")
 
-let total;
-let operator;
-let numA = null
-let numB = null
-let currentNum = []
+let history = {
+    numHist: [],
+    opHist: [],
+    totalsHist: [],
+}
 
+let calc = {
+    numA: null,
+    numB: null,
+    operator: null,
+    total: null,
+    currentNum: [],
+}
+
+const calcReset = {
+    numA: null,
+    numB: null,
+    operator: null,
+    total: null,
+    currentNum: [],
+}
+const historyReset = {
+    numHist: [],
+    opHist: [],
+    totalsHist: [],
+}
+
+//----CALCULTATIONS---//
 
 function add(a, b) {
-    total = a + b
-    return total
+    calc.total = a + b
+    return calc.total
 }
 
 function subtract(a, b) {
-    total = a - b
-    return total
+    calc.total = a - b
+    return calc.total
 }
 
 function multiply(a, b){
-    total = a * b
-    return total
+    calc.total = a * b
+    return calc.total
 }
 
 function divide(a, b){
-    total = a / b
-    return total
+    calc.total = a / b
+    return calc.total
 }
 
-function operate(operator, numA, numB){
-    switch (operator){
+function operate(){
+
+    switch (calc.operator){
         case "+":
-            add(numA, numB)
+            add(calc.numA, calc.numB)
             break;
         case "-":
-            subtract(numA, numB)
+            subtract(calc.numA, calc.numB)
             break;
         case "x":
-            multiply(numA, numB)
+            multiply(calc.numA, calc.numB)
             break;
         case "&#247":
-            divide(numA, numB)
+            divide(calc.numA, calc.numB)
             break;
     }
-    return total
+
+    return calc.total
 }
 
 function saveInput(e){
     let num = e.target.textContent
-    currentNum.push(num)
-    return currentNum
+    calc.currentNum.push(num)
+    return calc.currentNum
 }
 
 function saveOperator(e){
-    operator = e.target.textContent
-    return operator
+    calc.operator = e.target.textContent
+    return calc.operator
 }
 
 function saveNum(){
-    if (numA === null){
-        numA = Number(currentNum.join(""))
+    if (calc.numA === null){
+        calc.numA = Number(calc.currentNum.join(""))
     } else {
-        numB = Number(currentNum.join(""))
+        calc.numB = Number(calc.currentNum.join(""))
     }
-    currentNum = []
+    calc.currentNum = []
 }
+
+function saveHistory() {
+    history.numHist.push(calc.numA, calc.numB)
+    history.totalsHist.push(calc.total)
+    history.opHist.push(calc.operator)
+}
+
+function resetSingleValue(...values){
+    values.forEach(value => () => {
+        if (value.isArray()) {
+            value = []
+        } else {
+            value = null
+        }}
+)}
+
+function calcCheck(){
+    if (calc.numA != null && calc.numB != null && calc.operator != null) {
+        operate()
+        saveHistory()
+        resetSingleValue(calc.numA, calc.numB, calc.operator)
+        calc.numA = calc.total
+    }
+    return
+}
+
+//---DISPLAY---//
+
 
 
 calcNumbers.forEach(number => number.addEventListener("click", saveInput))
+
 operators.forEach(operator => operator.addEventListener("click", (e) => {
-    saveNum();
+    calcCheck()
+    saveNum()
     saveOperator(e)
 }))
+
+
 equalsButton.addEventListener("click", () => {
     saveNum()
-    operate(operator, numA, numB)
+    operate()
 })
 
 
