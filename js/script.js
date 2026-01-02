6bca4aaeb06306f36736423cd6c0c020f7ed5d57
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-01-02 03:56:59 +0000

    Insert basic math functions
    Four new functions created for add, subtract, multiply, and divide.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    total = a + b
+    return total
+}
+
+function subtract(a, b) {
+    total = a - b
+    return total
+}
+
+function multiply(a, b){
+    total = a * b
+    return total
+}
+
+function divide(a, b){
+    total = a / b
+    return total
+}
